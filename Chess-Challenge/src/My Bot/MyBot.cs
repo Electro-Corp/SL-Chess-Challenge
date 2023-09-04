4cf654ef80583a1eee0fa252195771df8adfe735
Electro-Corp <chinmaytanmay@outlook.com>
2023-09-03 20:59:32 -0600

    funny bot that just checks if a move has a peice to be captureed and does that

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,34 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
+    
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move move = new Move();
+        // Check all values of being captured
+        Move bestCapture = new Move();
+        foreach(Move m in moves)
+        {
+            if(m.CapturePieceType != PieceType.None)
+            {
+                if(m.CapturePieceType > bestCapture.CapturePieceType)
+                {
+                    bestCapture = m;
+                }
+            }
+        }
+
+        if (!bestCapture.IsNull)
+        {
+            move = bestCapture;
+        }
+        else
+        {
+            move = moves[0];
+        }
+        return move;
     }
 }
